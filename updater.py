1c29780ccda0e179b01eaf10c236bac0db02641e
TheCyberDevlopper <56231137+TheCyberDevlopper@users.noreply.github.com>
2021-05-07 20:52:00 +0200

    Add API

---
+++ updater.py
@@ -0,0 +1,54 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+import datetime
+import requests
+
+load_dotenv(dotenv_path='config/.env')
+
+TOKEN = os.getenv('TOKEN')
+WTOKEN = os.getenv('UPDATE_TOKEN')
+prefix = '*'
+bot = commands.Bot(command_prefix=prefix)
+bot.remove_command('help')
+
+#########################################################
+#         Bot devlopped by Moi#5013 on discord          #
+#########################################################
+#   __  __         _    _  _    ____    ___   _  _____  #
+#  |  \/  |  ___  (_) _| || |_ | ___|  / _ \ / ||___ /  #
+#  | |\/| | / _ \ | ||_  ..  _||___ \ | | | || |  |_ \  #
+#  | |  | || (_) || ||_      _| ___) || |_| || | ___) | #
+#  |_|  |_| \___/ |_|  |_||_|  |____/  \___/ |_||____/  #
+#                                                       #
+#########################################################
+
+def refresh():
+    num2 = 0
+    tmember = 0
+    for i in bot.guilds:
+        server = bot.guilds[num2]
+        if server.id in [834445430669574234, 798857253934858269]:
+            pass
+        else:
+            tmember = tmember + int(server.member_count)
+
+        num2 = num2 + 1
+
+    return {"servers":len(bot.guilds),"users":tmember}
+
+@bot.event
+async def on_ready():
+    print('I am ready')
+
+
+@bot.command()
+async def update(ctx):
+    datet = datetime.datetime.today()
+
+    data = refresh()
+    requests.get("http://127.0.0.1:5000/update?token="+WTOKEN, params=data)
+    print("DATA POSTED")
+
+bot.run(TOKEN)
--- web.py
+++ web.py
@@ -1,16 +1,13 @@
-import discord
+
 import os
-from discord.ext import commands
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 
 load_dotenv(dotenv_path='config/.env')
 
 app = Flask(__name__)
 TOKEN = os.getenv('TOKEN')
-prefix = 'fm!'
-bot = commands.Bot(command_prefix=prefix)
-bot.remove_command('help')
+WTOKEN = os.getenv('UPDATE_TOKEN')
 
 #########################################################
 #         Bot devlopped by Moi#5013 on discord          #
@@ -21,13 +18,25 @@ bot.remove_command('help')
 #  | |  | || (_) || ||_      _| ___) || |_| || | ___) | #
 #  |_|  |_| \___/ |_|  |_||_|  |____/  \___/ |_||____/  #
 #                                                       #
-########################################################
+#########################################################
+
+jfile = {"servers":"DATA-NOT-UPDATED", "users":"DATA-NOT-UPDATED"}
 
 @app.route('/')
 def index():
-    jfile = {"servers":"EXAMPLE", "users":"EXAMPLE"}
-    jfile["servers"] = len(bot.guilds)
     return jfile
+
+@app.route('/update')
+def update():
+    token = request.args.get('token')
+    servers = request.args.get('servers')
+    users = request.args.get('users')
+    if token == WTOKEN:
+        jfile['servers'] = servers
+        jfile['users'] = users
+        return jfile
+    else:
+        return {"error":"ACCES DENIED"}
 
 if __name__ == "__main__":
     app.run()
