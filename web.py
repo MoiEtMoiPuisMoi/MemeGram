5ad17b3f8f3ddc3a94028d2187c938718695c147
TheCyberDevlopper <56231137+TheCyberDevlopper@users.noreply.github.com>
2021-07-09 16:42:27 +0200

    Add Read Option Delete Stats

--- web.py
+++ web.py
@@ -1,12 +1,9 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask, request, render_template
+import random
+from meme import Meme
 
-load_dotenv(dotenv_path='config/.env')
 
 app = Flask(__name__)
-TOKEN = os.getenv('TOKEN')
-WTOKEN = os.getenv('UPDATE_TOKEN')
 
 #########################################################
 #         Bot devlopped by Moi#5013 on discord          #
@@ -19,28 +16,20 @@ WTOKEN = os.getenv('UPDATE_TOKEN')
 #                                                       #
 #########################################################
 
-jfile = {"servers":"None", "users":"None"}
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html', users=jfile["users"], servers=jfile["servers"])
+    return render_template('index.html')
 
-@app.route('/api')
-def api():
-    return jfile
+@app.route('/read')
+def index():
+    meme = Meme()
+    meme2 = Meme()
+    meme3 = Meme()
 
-@app.route('/update')
-def update():
-    token = request.args.get('token')
-    servers = request.args.get('servers')
-    users = request.args.get('users')
-    if token == WTOKEN:
-        jfile["servers"] = servers
-        jfile["users"] = users
-        return jfile
-    else:
-        return {"error":"ACCES DENIED"}
+    return render_template('read.html', reddit_name=meme.subreddit, image=meme.url, upvote=meme.ups, comments=random.randint(100, 1000), minute=random.randint(1, 59),
+                           reddit_name2=meme2.subreddit, image2=meme2.url, upvote2=meme2.ups, comments2=random.randint(100, 1000), minute2=random.randint(1, 59),
+                           reddit_name3=meme3.subreddit, image3=meme3.url, upvote3=meme3.ups, comments3=random.randint(100, 1000), minute3=random.randint(1, 59))
 
 if __name__ == "__main__":
     app.run()
